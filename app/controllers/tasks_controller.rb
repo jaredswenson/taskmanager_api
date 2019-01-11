82cd53901234042a29d4e876ccc235e3f7804238
Jared Swenson <jared@foreup.com>
2019-01-10 22:39:55 -0700

    made changes to task controller to order tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,18 +50,28 @@ class TasksController < ApplicationController
       end
       Task.update(id, is_completed: true)
     end
-    
-    
   end
 
   private
   def tasks(user, parent)  
-      parents = Task.where(user_id: user, parent_id: 0, is_completed: false)
-      children =  Task.where(user_id: user, is_completed: false)
+      @parents = Task.where(user_id: user, parent_id: 0, is_completed: false)
+      # date nonsense till I change date to date rather than string
+      @parents.each do |item|
+        item.due_date = Date.strptime(item.due_date, '%m/%d/%Y')
+        puts "Parent #{item.due_date}"
+      end
+      @sorted = @parents.sort_by { |parent| parent["due_date"].split('/').reverse }
+      @sorted.each do |task|
+        task.due_date = Date.parse(task.due_date)
+        puts "Parent #{task.due_date}"
+      end
+      # end date nonsense
+      @children =  Task.where(user_id: user, is_completed: false).where.not(parent_id: 0).sort_by{ |child| child["time_estimate"] };
+
       render json: {
         message: 'got tasks',
-        parentTasks: parents.order(:due_date),
-        childTasks: children.where.not(parent_id: 0)
+        parentTasks: @sorted,
+        childTasks: @children
       }
   end
 
