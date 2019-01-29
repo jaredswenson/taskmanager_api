47668ce4eadfa67d91ceab54e05d8e29203f3044
Jared Swenson <jared@foreup.com>
2019-01-29 16:49:38 -0700

    sendgrid test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,9 @@ class UsersController < ApplicationController
     mail = SendGrid::Mail.new(from, subject, to, content)
 
 
-    sg = SendGrid::API.new(api_key: 'SENDGRID_API_KEY')
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts "SENDGRID"
     puts response.status_code
     puts response.body
     puts response.headers
